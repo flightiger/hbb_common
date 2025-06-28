32f343ad3adc598feaf3d079aaaacf2decd220cf
flightiger <8120182@qq.com>
2025-06-28 21:35:09 +0800

    change fork my

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["www.rapixel.tech"];
+pub const RS_PUB_KEY: &str = "UD5XStvzOtSkyYz2hF4asw1Ono27expZUpsfTsMvGhY=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
