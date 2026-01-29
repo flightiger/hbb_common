02764b35de0e56664847ac6c495781c2700c9f93
flightiger <8120182@qq.com>
2026-01-29 10:57:17 +0800

    upjnspow=202601281123_shsvwhgs

--- src/config.rs
+++ src/config.rs
@@ -106,8 +106,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["www.rootman.top"];
-pub const RS_PUB_KEY: &str = "eIFRHvzHy2i+TyM1QbGosXSo7sIfbtX9Zf1F2VNvwug=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["www.jdeegman.top"];
+pub const RS_PUB_KEY: &str = "hdhedbevM1dekjdehbedhejdvd9Zf1F2VNvwug=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
